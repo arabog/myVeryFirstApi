5e8717182640116587a870351c512eff2b0ab6af
iCodeRx <arababs2016@gmail.com>
2021-04-01 13:54:28 +0100

    correct error

--- public/js/main.js
+++ public/js/main.js
@@ -4,7 +4,7 @@ async function getApi() {
           let inputVal = document.querySelector("input").value;
 
           try{
-                    let output = await fetch(`http://localhost:8000/api/${inputVal}`)
+                    let output = await fetch(`https://nigeriawesternstates.herokuapp.com/api/${inputVal}`)
                     let finalOutput = await output.json()
           
                     document.querySelector('h2').innerText = finalOutput.governor
